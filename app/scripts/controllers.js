595cbb12b9577862cc1d936f8613d39cdafeda2e
emilio.gozo <emilio.gozo@obf.ateneo.edu>
2016-03-26 13:20:36 -0400

    Handling Errors in Client-Server Communication using

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -8,9 +8,16 @@ angular.module('confusionApp')
   $scope.filtText = '';
   $scope.showDetails = false;
 
-  $scope.showMenu = true;
+  $scope.showMenu = false;
   $scope.message = "Loading ...";
-  $scope.dishes = menuFactory.getDishes().query();
+  menuFactory.getDishes().query(
+    function(response) {
+      $scope.dishes = response;
+      $scope.showMenu = true;
+    },
+    function(response) {
+      $scope.message = "Error: " + response.status + " " + response.statusText;
+    });
 
 
   $scope.select = function(setTab) {
@@ -86,15 +93,24 @@ angular.module('confusionApp')
 
 .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
-  $scope.showDish = true;
+  $scope.showDish = false;
   $scope.message = "Loading ...";
   $scope.dish = menuFactory.getDishes().get({
-    id: parseInt($stateParams.id, 10)
-  });
+      id: parseInt($stateParams.id, 10)
+    })
+    .$promise.then(
+      function(response) {
+        $scope.dish = response;
+        $scope.showDish = true;
+      },
+      function(response) {
+        $scope.message = "Error: " + response.status + " " + response.statusText;
+      }
+    );
 
 }])
 
-.controller('DishCommentController', ['$scope', function($scope) {
+.controller('DishCommentController', ['$scope', 'menuFactory', function($scope, menuFactory) {
 
   $scope.mycomment = {
     rating: 5,
@@ -104,14 +120,14 @@ angular.module('confusionApp')
   };
 
   $scope.submitComment = function() {
-
     $scope.mycomment.date = new Date().toISOString();
     console.log($scope.mycomment);
-
     $scope.dish.comments.push($scope.mycomment);
 
+    menuFactory.getDishes().update({
+      id: $scope.dish.id
+    }, $scope.dish);
     $scope.commentForm.$setPristine();
-
     $scope.mycomment = {
       rating: 5,
       comment: "",
@@ -136,11 +152,20 @@ angular.module('confusionApp')
   $scope.promotion = menuFactory.getPromotion(promoIdx);
   $scope.featLeader = corporateFactory.getLeader(featLeaderIdx);
 
-  $scope.showDish = true;
+  $scope.showDish = false;
   $scope.message = "Loading ...";
   $scope.dish = menuFactory.getDishes().get({
-    id: 0
-  });
+      id: 0
+    })
+    .$promise.then(
+      function(response) {
+        $scope.dish = response;
+        $scope.showDish = true;
+      },
+      function(response) {
+        $scope.message = "Error: " + response.status + " " + response.statusText;
+      }
+    );
 
 }])
 
